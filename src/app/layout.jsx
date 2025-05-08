7ab7f21965bcb26c032d2a53f0685023abf33107
Cortes64 <esteban01cortes@gmail.com>
2025-05-07 19:06:48 -0600

    Agregando elementos al navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,19 @@
 import { inter } from '@/app/ui/fonts'
 import Navbar from '@/components/Navbar';
 import './globals.css'
+import { Metadata } from 'next'
+
+export const metadata = {
+  title: 'Asistencias y Tutorías',
+  description: 'Sistema de gestión de asistencias y tutorías para estudiantes del Tecnológico de Costa Rica',
+}
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=logout" />
+      </head>
       <body className={`${inter.className}`}>
         <Navbar />
         {children}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,18 +11,20 @@ function Navbar() {
           <h1>Asistencias y Tutorías</h1>
         </Link>
         <ul className="flex gap-4">
-          <li className="text-lg font-bold text-blue-200 hover:text-blue-300 border-2 rounded-2xl border-blue-200 px-4 py-2">
-            <Link href="/login">Iniciar Sesion</Link>
-          </li>
-          <li className="text-lg font-bold text-blue-200 hover:text-blue-300 border-2 rounded-2xl border-blue-200 px-4 py-2">
-            <Link href="/register">Registrarse</Link>
+          <li className="text-lg font-bold text-blue-200 hover:text-blue-300">
+            <Link href="/login">
+              <span className="material-symbols-outlined align-middle">logout</span>
+            </Link>
           </li>
         </ul>
       </div>
       <div className="flex flex-row justify-center bg-cyan-800 p-1">
-        <ul>
+        <ul className="flex justify-between p-2 text-white font-bold text-lg">
           <li>
             <Link className="text-xl" href="/">Inicio</Link>
+          </li>
+          <li>
+            <Link className="text-xl" href="/asistencias">Buscar oferta</Link>
           </li>
         </ul>
       </div>
